8a9d8aa4293d062ab3616ca46a0b9d86f67501e0
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-08-06 21:18:00 +0800

    3.8*: phonebook backend step8

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
+morgan.token('body', (req, res) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    }
+  })
+
 const app = express()
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
 
 let persons = [

53e5d9aa9e3cca4dc059fa30a61831305422c436
Dariush Bahri <dazb@alumni.york.ac.uk>
2020-08-10 16:24:44 +0800

    uibuild

---
+++ build/precache-manifest.2fad523dd2ccb98306cb27cce4f30643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0c3d60077d655f2cc2d7bc80db37c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4115960a7a81a1be3667",
+    "url": "/static/css/main.1969721a.chunk.css"
+  },
+  {
+    "revision": "db977e8610798d8a546c",
+    "url": "/static/js/2.a64c161a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a64c161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4115960a7a81a1be3667",
+    "url": "/static/js/main.ad89b0a6.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
